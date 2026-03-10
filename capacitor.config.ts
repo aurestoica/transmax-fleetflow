0b78723e50a2fbe1975f052d31d34f0844b71131
Aure Stoica <stoica.aure@gmail.com>
2026-03-10 13:23:26 +0200

    Update Capacitor config for local build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.11f235b0c29444608f302da06cc4d702',
   appName: 'transmax-fleetflow',
   webDir: 'dist',
-  server: {
-    url: 'https://11f235b0-c294-4460-8f30-2da06cc4d702.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
 };
 
 export default config;
